2a09dd18dc3870f7d6306fb72b4f6ee511559a8b
Valerii Vasianovych <valerii.vasianovych.2003@gmail.com>
2025-12-15 00:49:26 +0100

    change in notifications

--- build/git_update.sh
+++ build/git_update.sh
@@ -10,8 +10,12 @@ do
   esac
 done
 
-git fetch --prune --unshallow 2>/dev/null
-CURRENT_VERSION=`git describe --abbrev=0 --tags 2>/dev/null`
+# Fetch all tags from remote
+git fetch --tags --prune 2>/dev/null || true
+git fetch --unshallow 2>/dev/null || true
+
+# Get the latest tag by version number (not by commit date)
+CURRENT_VERSION=$(git tag -l | grep -E '^v[0-9]+\.[0-9]+\.[0-9]+$' | sort -V | tail -1)
 
 if [[ $CURRENT_VERSION == '' ]]
 then
@@ -19,7 +23,9 @@ then
 fi
 echo "Current Version: $CURRENT_VERSION"
 
-CURRENT_VERSION_PARTS=(${CURRENT_VERSION//./ })
+# Remove 'v' prefix and split version
+VERSION_NUMBER=${CURRENT_VERSION#v}
+CURRENT_VERSION_PARTS=(${VERSION_NUMBER//./ })
 
 VNUM1=${CURRENT_VERSION_PARTS[0]}
 VNUM2=${CURRENT_VERSION_PARTS[1]}
@@ -27,7 +33,7 @@ VNUM3=${CURRENT_VERSION_PARTS[2]}
 
 if [[ $VERSION == 'major' ]]
 then
-  VNUM1=v$((VNUM1+1))
+  VNUM1=$((VNUM1+1))
 elif [[ $VERSION == 'minor' ]]
 then
   VNUM2=$((VNUM2+1))
@@ -39,19 +45,24 @@ else
   exit 1
 fi
 
-NEW_TAG="$VNUM1.$VNUM2.$VNUM3"
+NEW_TAG="v$VNUM1.$VNUM2.$VNUM3"
 echo "($VERSION) updating $CURRENT_VERSION to $NEW_TAG"
 
-GIT_COMMIT=`git rev-parse HEAD`
-NEEDS_TAG=`git describe --contains $GIT_COMMIT 2>/dev/null`
-
-if [ -z "$NEEDS_TAG" ]; then
-  echo "Tagged with $NEW_TAG"
-  git tag $NEW_TAG
-  git push --tags
-  git push
+# Check if the new tag already exists
+GIT_COMMIT=$(git rev-parse HEAD)
+if git rev-parse "$NEW_TAG" >/dev/null 2>&1; then
+  TAG_COMMIT=$(git rev-parse "$NEW_TAG")
+  if [ "$TAG_COMMIT" = "$GIT_COMMIT" ]; then
+    echo "Tag $NEW_TAG already exists on current commit, reusing it"
+  else
+    echo "ERROR: Tag $NEW_TAG already exists on different commit ($TAG_COMMIT), current commit is $GIT_COMMIT"
+    echo "This should not happen. Please check your tags."
+    exit 1
+  fi
 else
-  echo "Already a tag on this commit"
+  echo "Tagging commit $GIT_COMMIT with $NEW_TAG"
+  git tag $NEW_TAG
+  git push origin $NEW_TAG
 fi
 
 echo "git-tag=$NEW_TAG" >> $GITHUB_OUTPUT
